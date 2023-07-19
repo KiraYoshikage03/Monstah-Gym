9eb5d6655a22b5387c92e416940cd56d1dd15d7e
KiraYoshikage03 <xdbernard0018@gmail.com>
2023-07-19 14:33:59 +0800

    Updated settings.py

--- gymsystem/gymsystem/settings.py
+++ gymsystem/gymsystem/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-wn_k2q%fl-l9-4(j=w6cwy0txrn)4s*vau&goz(ggmulme!+&1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
